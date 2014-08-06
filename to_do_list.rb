40365a25e955a29a6127867de8ab8dcbc3a1f8b1
Meghan Lindsley & Randy Leighton <student@epicodus.com>
2014-08-06 16:44:14 -0700

    add task, list task, clear database features added

--- to_do_list.rb
+++ to_do_list.rb
@@ -1,18 +1,27 @@
 require 'pg'
 require './lib/task'
+require './lib/list'
 
 DB = PG.connect({:dbname => 'to_do'})
 
 def main_menu
   loop do
     puts "[ == Task List Main Menu ==]"
-    create_list
-    # puts "[c] Create a new List"
+    puts "[z] clear the databases"
+    puts "[c] Create a new List"
     # puts "[l] View Lists"
     puts "[a] Add Tasks"
+    puts "[x] Exit home"
+    puts "[l] List Tasks"
     menu_choice = gets.chomp
     if menu_choice == 'a'
       add_task
+    elsif menu_choice == 'c'
+      create_list
+    elsif menu_choice == 'l'
+      list_tasks
+    elsif menu_choice == 'z'
+       detonate
     elsif menu_choice == 'x'
       puts "Thanks."
       exit
@@ -23,11 +32,27 @@ def main_menu
 end
 
 def create_list
-@current_list = List.new("Stuff", 1)
+List.new("Stuff",1).save
+@current_list = List.all.last
+puts "Happiness is: #{@current_list.name}"
 end
 
 def add_task
 puts "\n\nEnter task for the list: "
 task_choice = gets.chomp
+saved_task = Task.new(task_choice, @current_list.id).save
 
 end
+
+def list_tasks
+  Task.all.each do |task|
+    puts task.name
+  end
+end
+
+def detonate
+  DB.exec("DELETE FROM tasks *;")
+  DB.exec("DELETE FROM lists *;")
+end
+
+main_menu
